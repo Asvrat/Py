70cc9293246a83f2ef2b9cd9f7702f4626e71c8e
Asvrat <manulandx@gmail.com>
2023-04-18 19:05:14 +0300

    Test commit

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
-print("Hello, World!")
+# print("Hello, World!")
 
-def expon(n):
-    return lambda a : a ** n
-x = expon(5)
-print(x(2))
+# def expon(n):
+#     return lambda a : a ** n
+# x = expon(5)
+# print(x(2))
+
+print("Commited!")
